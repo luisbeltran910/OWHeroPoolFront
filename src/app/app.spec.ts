f04480f78d2c44b832ee196a97b9d457abbed3a1
Luis Beltran <luisbeltran991@gmail.com>
2026-09-13 13:02:39 -0700

    feat: Updated header  and logo to be more simpler

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -21,6 +21,6 @@ describe('App', () => {
     const fixture = TestBed.createComponent(App);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('OW Hero Pool');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hero Pool');
   });
 });
